af157e9e8145f836dabd4e902df9164c6e16bbb9
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-15 11:01:18 +0100

    feat: install tailwind, create file app.routing, add code register.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [AppComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
