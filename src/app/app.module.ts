e2ccb8c9a2af31af668a901401644eed0dbc31b2
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-15 10:03:52 +0100

    feat: create component register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
