4be66f61830cbaa9b6b2555b0be4fa118ec20ad3
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-15 15:03:31 +0100

    feat: create routing dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
@@ -8,6 +9,7 @@ const routes: Routes = [
 { path: 'register', component: RegisterComponent},
 { path: '', component: HomeComponent},
 { path: 'login', component: LoginComponent},
+{ path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
