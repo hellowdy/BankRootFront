03704a2fff011f055312100ad0bbade5427b825c
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-15 16:22:54 +0100

    feat: add routing account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AccountComponent } from "./account/account.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
@@ -12,6 +13,7 @@ const routes: Routes = [
 { path: 'login', component: LoginComponent},
 { path: 'dashboard', component: DashboardComponent},
 { path: 'transaction', component: TransactionComponent},
+{ path: 'account', component: AccountComponent},
 ];
 
 @NgModule({
