c8828b21d920249df6defc269d920f81f9484ef0
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-15 15:41:19 +0100

    feat: add routing transaction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { TransactionComponent } from "./transaction/transaction.component";
 
 const routes: Routes = [
 { path: 'register', component: RegisterComponent},
 { path: '', component: HomeComponent},
 { path: 'login', component: LoginComponent},
 { path: 'dashboard', component: DashboardComponent},
+{ path: 'transaction', component: TransactionComponent},
 ];
 
 @NgModule({
