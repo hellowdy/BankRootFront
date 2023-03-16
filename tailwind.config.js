970586dee0b7567fc501e1e74a36be824171e3a5
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-16 09:51:29 +0100

    feat: add navbar on all page and add logo.png

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "navbr": '#D0D1D2',
+      }
+    },
   },
   plugins: [],
 }
