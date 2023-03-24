3c640027d469d426d2ff9a4d9bc36aae92ded311
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-24 14:09:58 +0100

    feat: add method logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+  constructor(private storageService: StorageService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+  }
+
+  logout(){
+    this.storageService.clean();
+    return this.router.navigateByUrl('/login');
+  }
 }
