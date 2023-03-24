1c0efd779d2c7a0dc7ab57c3642951ceafa2f6c8
Elodie mongellaz <mongellaz.e@gmail.com>
2023-03-24 14:47:39 +0100

    feat: add methods getUserById and getProfile

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -1,31 +1,39 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-interface LoginResponse {
-  token: string;
-}
+// interface LoginResponse {
+//   token: string;
+// }
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: HttpClient) {
+  constructor(private httpClient: HttpClient) {
   }
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     })
   };
 
   getUsers() {
-    return this.http.get("/user", this.httpOptions);
+    return this.httpClient.get("/user", this.httpOptions);
   }
 
   createUser(user: any) {
-    return this.http.post("/auth/register", user, this.httpOptions)
+    return this.httpClient.post("/auth/register", user, this.httpOptions)
+  }
+
+  getUserById(id: number) {
+    return this.httpClient.get(`/user/${id}`, this.httpOptions);
+  }
+
+  getProfile(){
+    return this.httpClient.get("/user/find/profile", this.httpOptions);
   }
   
   // login(formData: any){
-  //   return this.http.post<LoginResponse>(`/login`, formData, this.httpOptions);
+  //   return this.http.post(`/login`, user, this.httpOptions);
   // }
 }
